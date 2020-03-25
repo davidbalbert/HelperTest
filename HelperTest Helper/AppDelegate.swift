5a906e78b3067f236696f014a251ea5c895260bc
David Albert <davidbalbert@gmail.com>
2020-03-25 08:24:32 -0400

    Initial commit

---
+++ HelperTest Helper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HelperTest Helper
+//
+//  Created by David Albert on 3/24/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
